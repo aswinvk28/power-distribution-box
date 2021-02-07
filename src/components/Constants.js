437d0c0e9264a0b45acd094692dce3023748064b
Aswin Vijayakumar <aswinkvj@gmail.com>
2021-02-07 13:23:37 +0000

    styling changes to distribution box

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -1,6 +1,14 @@
+const ElementType = {
+    OUTPUTS: 'Outputs',
+    FRONTS: 'Front',
+    ADDONS: 'Addons',
+    INPUTS: 'Inputs'
+};
+
 const Constants = {
     useLocalStorage: true,
-    gridSize: 16
+    gridSize: 16,
+    ElementType: ElementType
 };
 
 export default Constants;
